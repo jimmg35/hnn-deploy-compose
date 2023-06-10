98d08643033adbb932c4a90b837a84e193257e7b
jimmg35 <jim60308@gmail.com>
2023-06-10 10:23:58 +0800

    feat: 完成imageLayer設定

--- database-entrypoint-dev/database.entrypoint.sh
+++ database-entrypoint-dev/database.entrypoint.sh
@@ -6,16 +6,18 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $APP_DB_USER;
   \connect $APP_DB_NAME $APP_DB_USER
   BEGIN;
-    CREATE TABLE IF NOT EXISTS event (
-	  id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(id) = 26) PRIMARY KEY,
-	  aggregate_id CHAR(26) NOT NULL CHECK (CHAR_LENGTH(aggregate_id) = 26),
-	  event_data JSON NOT NULL,
-	  version INT,
-	  UNIQUE(aggregate_id, version)
-	);
-	CREATE INDEX idx_event_aggregate_id ON event (aggregate_id);
 
-	
+	\i /docker-entrypoint-initdb.d/seed/hnn-database.sql
 
   COMMIT;
 EOSQL
+
+
+# sleep 1m	
+
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+#   \connect $APP_DB_NAME $APP_DB_USER
+#   BEGIN;
+# 	\i /docker-entrypoint-initdb.d/seed/ndvi2018.sql
+#   COMMIT;
+# EOSQL
